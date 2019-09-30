fc3bcd36b7e26e336a223b7c44690811922e7d37
arachnothrone <js1000avf@gmail.com>
2019-09-29 22:45:34 -0400

    Add heaader file

---
+++ weathermon.h
@@ -0,0 +1,24 @@
+// headerfile
+
+#ifndef __WEATHERMON_H__
+#define __WEATHERMON_H__
+
+typedef struct {
+  char symbol;// = ' ';
+  bool dir;// = false;
+  int x_begin;// = 9;
+  int x_end;// = 15;
+  int x_old;// = 12;
+  int x_coord;// = 12; 
+} Arrow;
+
+void taskOneFunc();
+void taskTwoFunc();
+void taskThreeFunc();
+void taskFourFunc();
+void arrowStep(Arrow *self);
+void sdCardProgram();
+
+
+#endif
+
