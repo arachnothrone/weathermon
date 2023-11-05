2135f33ebc8510f66979c9e04b142d93ae12a6c0
arachnothrone <arachnothrone@gmail.com>
2023-11-04 20:08:36 -0400

    ArduinoData structure updated

--- rpi_wserver/rpiwserver.h
+++ rpi_wserver/rpiwserver.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include <string>
+#include <stdint.h>
 
 /* Search direction */
 #define FORWARD 1
 #define BACKWARD 0
+
+typedef struct {
+    uint16_t totalNrOfRestarts;
+    uint16_t successCounter;
+    uint16_t rxBufferOverrunCntr;
+} STATS_T;
 
 class ArduinoData {
 public:
@@ -17,5 +24,11 @@ private:
     // int _id;
     // int _value;
     // int _timestamp;
+    int8_t      _temperature;
+    uint8_t     _humidity;
+    uint16_t    _pressure;
+    uint16_t    _ambientLight;
+    uint16_t    _vcc;
+    STATS_T     _stats;
     std::string _dataString;
 };
